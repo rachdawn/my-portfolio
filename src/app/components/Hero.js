1f9808cc541f0aa0ae9ec6a01892424513e83aca
Rachel Little <racheldawnlittle@gmail.com>
2024-04-08 16:29:03 -0600

    create hero component

---
+++ src/app/components/Hero.js
@@ -0,0 +1,39 @@
+import styles from "../page.module.css";
+import Typography from "@mui/material/Typography";
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <Typography
+        variant="body"
+        component="h1"
+        sx={{
+          fontFamily: "monospace",
+          color: "#707070",
+          letterSpacing: ".3rem",
+          fontSize: "3.5dvb",
+          fontWeight: 500,
+          textWrap: "nowrap",
+        }}
+      >
+        my name is rachel little,
+      </Typography>
+      <Typography
+        variant="body"
+        component="h2"
+        gutterBottom
+        sx={{
+          fontFamily: "monospace",
+          color: "#707070",
+          letterSpacing: ".3rem",
+          fontSize: "4dvb",
+          fontWeight: 700,
+          textTransform: "uppercase",
+          textWrap: "nowrap",
+        }}
+      >
+        I have BIG ideas.
+      </Typography>
+    </section>
+  );
+}
