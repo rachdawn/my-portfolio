a137532ed9675f41fd77ba8e7043173369998ddd
Rachel Little <racheldawnlittle@gmail.com>
2024-04-08 16:27:45 -0600

    add MUI container to match app bar responsiveness, add project cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,22 @@
 import styles from "./page.module.css";
 import Hero from "./components/Hero";
+import ProjectCard from "./components/ProjectCard";
+// MUI
+import Container from "@mui/material/Container";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <Hero />
-      <section>
-      </section>
+        <Container maxWidth="xl">
+            <Hero />
+        <div className={styles.projects}>
+            <h2>Recent Projects</h2>
+            <hr />
+            <div className={styles.projectCards}>
+            <ProjectCard />
+            </div>
+        </div>
+        </Container>
     </main>
   );
 }
