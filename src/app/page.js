19a3e3a3be1da85bae7d39114a3316e6b8cfc693
Rachel Little <racheldawnlittle@gmail.com>
2024-04-15 15:00:55 -0600

    add about section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
 import ProjectCard from "./components/ProjectCard";
 // MUI
 import Container from "@mui/material/Container";
@@ -7,16 +8,21 @@ import Container from "@mui/material/Container";
 export default function Home() {
   return (
     <main className={styles.main}>
-        <Container maxWidth="xl">
-            <Hero />
-        <div className={styles.projects}>
-            <h2>Recent Projects</h2>
-            <hr />
-            <div className={styles.projectCards}>
-            <ProjectCard />
-            </div>
+      <Container maxWidth="xl" sx={{ pb: 8 }}>
+        <Hero />
+        <div className={styles.about}>
+          {/* <h2>About Me</h2> */}
+          {/* <hr /> */}
+          <AboutMe />
         </div>
-        </Container>
+        <div className={styles.projects}>
+          <h2>Recent Projects</h2>
+          <hr />
+          <div className={styles.projectCards}>
+            <ProjectCard />
+          </div>
+        </div>
+      </Container>
     </main>
   );
 }
